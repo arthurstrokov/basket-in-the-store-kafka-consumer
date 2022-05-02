044df1a5e64cb13c9a7f75b4e111c02e29564829
Arthur Strokov <arthurstrokov@gmail.com>
2022-05-02 17:44:49 +0300

    up test

--- src/test/java/com/gmail/arthurstrokov/consumer/BasketInTheStoreKafkaConsumerApplicationTestsService.java
+++ src/test/java/com/gmail/arthurstrokov/consumer/BasketInTheStoreKafkaConsumerApplicationTestsService.java
@@ -1,13 +1,20 @@
 package com.gmail.arthurstrokov.consumer;
 
+import com.gmail.arthurstrokov.consumer.service.KafkaConsumerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class BasketInTheStoreKafkaConsumerApplicationTestsService {
+class KafkaConsumerApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    KafkaConsumerService kafkaConsumerService;
 
+    @Test
+    void contextLoads() {
+        assertNotNull(kafkaConsumerService);
+    }
 }
